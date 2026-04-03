2a49e7440fbaba823cef3e0571c71ec7ce39c2fd
Dhuvie <thedhruvbajaj@gmail.com>
2026-04-03 11:20:00 +0530

    feat: initial DIP engine foundation

--- src/lib/dipEngine.ts
+++ src/lib/dipEngine.ts
@@ -1,9 +1,1 @@
-// April Mega Overhaul
-export class AdvancedDIPEngine {
-  fullEnhancementPipeline(imageData: ImageData, settings: any) {
-    console.log('Executing complete professional enhancement pipeline');
-    let result = imageData;
-    // Brightness, Contrast, Unsharp, Noise Reduction, Cinematic Grade, etc.
-    return result;
-  }
-}
+export class DIPEngine { /* core methods */ }
