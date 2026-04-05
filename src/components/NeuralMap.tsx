94933c56be889a61f063c20ea1e0ea39648ce626
Dhuvie <thedhruvbajaj@gmail.com>
2026-04-05 11:45:00 +0530

    feat: Neural Map overlay component

--- src/components/NeuralMap.tsx
+++ src/components/NeuralMap.tsx
@@ -1,14 +1,9 @@
 'use client';
-import React from 'react';
-
 export default function NeuralMap({ detections = [] }) {
   return (
     <div className="absolute inset-0 pointer-events-none">
-      {detections.map((det, i) => (
-        <div key={i} className="absolute border-2 border-lime-400" 
-             style={{left: det.bbox?.x || 0, top: det.bbox?.y || 0, width: det.bbox?.width || 100, height: det.bbox?.height || 100}}>
-          <div className="bg-black/70 text-white text-xs px-1 absolute -top-5">{det.label}</div>
-        </div>
+      {detections.map((d, i) => (
+        <div key={i} className="absolute border-2 border-lime-400" style={{left: d.x || 0, top: d.y || 0}} />
       ))}
     </div>
   );
