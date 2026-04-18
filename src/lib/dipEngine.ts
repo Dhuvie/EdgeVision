86a57b439dcd0d228f6744845190f5c123b1ccff
Dhuvie <thedhruvbajaj@gmail.com>
2026-04-18 16:40:00 +0530

    feat: add histogram equalization

--- src/lib/dipEngine.ts
+++ src/lib/dipEngine.ts
@@ -1,2 +1,5 @@
 export class DIPEngine { /* core methods */ }
 export function applyCinematicPresets() { /* 10 presets */ }
+export function applyHistogramEqualization(imageData: ImageData) {
+  console.log('Histogram equalization applied for better contrast');
+}
